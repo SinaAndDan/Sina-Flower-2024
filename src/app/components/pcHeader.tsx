5c88051c4b87273d654abc0cc0ee51abb279be31
sinaKarimi <karimicna@gmail.com>
2024-08-17 03:25:27 +0330

    just made a foundation of pc-navbar

---
+++ src/app/components/pcHeader.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PcHeader = () => {
+  return (
+    <nav className="sm:py-4 container mx-auto flex justify-between">
+      <div className="flex justify-between basis-1/2">
+        <a href="" className="">
+          Logo
+        </a>
+        <div></div>
+        <a href="">Home</a>
+        <a href="">Shop</a>
+        <a href="">About Us</a>
+      </div>
+      <div className="flex basis-1/3 justify-around place-self-end">
+        <a href="">search</a>
+        <a href="">card</a>
+      </div>
+    </nav>
+  );
+};
+
+export default PcHeader;
