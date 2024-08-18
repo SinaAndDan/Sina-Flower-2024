546f03694c3f77ab5b22e40f6e6231c014410521
Danial <thederekreigns1999@gmail.com>
2024-08-18 12:27:22 +0330

    Refactor components for improved cleanliness and leaner code; add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
 import PcHeader from "./components/pcHeader";
 import MobileMainPage from "./components/MobileMainPage/MobileMainPage";
-import MobileHeader from "./components/MobileMainPage/MobileHeader";
 
 export default function Home() {
   return (
     <>
       {/* <PcHeader /> */}
-      <MobileHeader />
-      <MobileMainPage />
-      <main className="flex min-h-screen flex-col items-center justify-between bg-slate-400"></main>
+
+      <main>
+        <MobileMainPage />
+      </main>
     </>
   );
 }
