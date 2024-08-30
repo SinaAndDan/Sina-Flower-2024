77ba66c6ca57b09e83e434143a82a160581f682c
sinaKarimi <karimicna@gmail.com>
2024-08-31 01:29:30 +0330

    fonts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import MainPage from "./components/MainPage/MainPage"
+import MainPage from "./components/MainPage/MainPage";
+import "../styles/fonts.css";
 
 export default function Home() {
   return (
-    <>
+    <div className="font-yekan">
       <MainPage />
-      </main>
-    </>
+    </div>
   );
 }
