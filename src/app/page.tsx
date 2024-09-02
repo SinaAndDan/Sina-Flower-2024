d9599d87df9f52dfab3527a92652abf7d93710b2
sinaKarimi <karimicna@gmail.com>
2024-09-02 12:20:00 +0330

    bug main page fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import MainPage from "./components/MainPage/MainPage"
+import MainPage from "./components/MainPage/MainPage";
 
 export default function Home() {
   return (
     <>
       <MainPage />
-      </main>
     </>
   );
 }
