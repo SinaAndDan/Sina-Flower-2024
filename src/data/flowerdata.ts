b3eacdeb3068c487ab24c6faa4a0ee49df03d935
Danial <thederekreigns1999@gmail.com>
2024-09-12 19:14:55 +0330

    Prices turned to toman

--- src/data/flowerdata.ts
+++ src/data/flowerdata.ts
@@ -5,37 +5,37 @@ const flowerData = [
   {
     id: 1,
     name: "شفلرا",
-    price: "40$",
+    price: "120,000 تومان",
     image: "/schefflera.jpg", // Path relative to the public folder
   },
   {
     id: 2,
     name: "داوودی",
-    price: "$15",
+    price: "180,000 تومان",
     image: "/Chrysanthemum.jpg", // Path relative to the public folder
   },
   {
     id: 3,
     name: "عروس",
-    price: "40$",
+    price: "200,000 تومان",
     image: "/whiteFlower.jpg", // Path relative to the public folder
   },
   {
     id: 4,
     name: "لیسیانتوس",
-    price: "40$",
+    price: "250,000 تومان",
     image: "/Prairie-gentian.jpg", // Path relative to the public folder
   },
   {
     id: 5,
     name: "شمشاد طلایی",
-    price: "40$",
+    price: "300,000 تومان",
     image: "/Variegated-euonymus.png", // Path relative to the public folder
   },
   {
     id: 6,
     name: "توت فرنگی",
-    price: "40$",
+    price: "350,000 تومان",
     image: "/strawberry-plant.jpg", // Path relative to the public folder
   },
 ];
@@ -44,31 +44,31 @@ const flowerFavorite = [
   {
     id: 1,
     name: "یه گل مشتی",
-    price: "40$",
+    price: "130,000 تومان",
     image: "/Variegated-euonymus.png", // Path relative to the public folder
   },
   {
     id: 2,
     name: "یه گل خیار",
-    price: "$15",
+    price: "190,000 تومان",
     image: "/PurpuleFlower.jpg", // Path relative to the public folder
   },
   {
     id: 3,
     name: "رز",
-    price: "40$",
+    price: "210,000 تومان",
     image: "/pinkFlower.jpg", // Path relative to the public folder
   },
   {
     id: 4,
     name: "رز",
-    price: "40$",
+    price: "270,000 تومان",
     image: "/ficus.png", // Path relative to the public folder
   },
   {
     id: 5,
     name: "رز",
-    price: "40$",
+    price: "320,000 تومان",
     image: "/green flowe.png", // Path relative to the public folder
   },
 ];
