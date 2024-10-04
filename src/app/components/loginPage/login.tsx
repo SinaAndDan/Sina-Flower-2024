9c1f2c9969484a433fbc7721ed74761a9644e709
sinaKarimi <karimicna@gmail.com>
2024-10-04 22:36:13 +0330

    image shape changed VI

--- src/app/components/loginPage/login.tsx
+++ src/app/components/loginPage/login.tsx
@@ -3,37 +3,41 @@ import Image from "next/image";
 
 const LogInPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center font-sahel">
-      <div className="image-containerII">
-        <Image
-          width={500}
-          height={500}
-          src="/loginPic.png"
-          alt="Full Screen Image"
-          className="-z-20"
-          priority={true} // Optional: Loads the image with high priority
-        />
-      </div>
-      <div className="relative w-full">
-        <div className="flex flex-col absolute left-1/2 translate-x-3">
-          <div className="text-greenlogIn text-2xl mt-12 font-semibold font-sahel ">
-            خوش آمدید
-          </div>
-          <p className="text-greenlogIn text-opacity-65 mt-3 text-xs">
-            به حساب خود وارد شوید
-          </p>
+    <>
+      <div className="flex flex-col items-center font-sahel h-screen">
+        <div className="relative h-2/5 w-screen md:h-[50vh] sm:h-[30vh]">
+          <Image
+            src="/loginPic.png"
+            alt="Header Image"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            className="w-full h-full"
+          />
         </div>
-        <Image
-          width={150}
-          height={150}
-          src="/sideimgV.png"
-          alt="Full Screen Image"
-          className="-z-20 absolute left-0 top-12"
-          priority={true} // Optional: Loads the image with high priority
-        />
+        <div className="relative w-full">
+          <div className="flex flex-col absolute left-1/2 translate-x-3">
+            <div className="text-greenlogIn text-2xl mt-12 font-semibold font-sahel ">
+              خوش آمدید
+            </div>
+            <p className="text-greenlogIn text-opacity-65 mt-3 text-xs">
+              به حساب خود وارد شوید
+            </p>
+          </div>
+          <Image
+            width={150}
+            height={150}
+            src="/sideimgV.png"
+            alt="Full Screen Image"
+            className="-z-20 absolute left-0 top-12"
+            priority={true} // Optional: Loads the image with high priority
+          />
+        </div>
       </div>
-      <input placeholder="messi" />
-    </div>
+      <div>
+        <input />
+      </div>
+    </>
   );
 };
 
