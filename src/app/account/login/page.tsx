0b5611a97448707e5521fcbcfa0d36cd629815ad
sinaKarimi <karimicna@gmail.com>
2024-10-04 17:03:13 +0330

    dont manipulate my code asshole

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,48 +1,8 @@
+import React from "react";
+import LogInPage from "../../components/loginPage/login";
 
-"use client";
-// pages/login.js
-import { useState } from "react";
-import { login } from "../../../../services/authService";
+const LogIn = () => {
+  return <LogInPage />;
+};
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setError(null);
-
-    try {
-      const data = await login(email, password);
-      console.log("Login successful", data);
-      // Save token to local storage or state management
-      localStorage.setItem("token", data.token);
-      // Redirect or show success message
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  return (
-    <form onSubmit={handleLogin}>
-      <h1>Login</h1>
-      {error && <p>{error}</p>}
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">Login</button>
-    </form>
-
-export default Login;
+export default LogIn;
