a6d75c2073f6a49fc7431fadd91b9d07646adbf0
sinaKarimi <karimicna@gmail.com>
2024-11-28 16:21:31 +0330

    navbar pc searchbar made

--- src/app/components/MainPage/PcNav.tsx
+++ src/app/components/MainPage/PcNav.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { IoSearch } from "react-icons/io5";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { motion } from "motion/react";
 
 const PcNav: React.FC = () => {
+  const [searchButton, setSearchButton] = useState(false);
+
   const pathname = usePathname();
   const getLinkClass = (path: string) => {
     console.log(path);
     return pathname === path ? "text-green" : "text-black";
   };
   console.log(pathname);
+
+  const searchButtonHandler = () => {
+    setSearchButton((prevSearchButton) => !prevSearchButton);
+  };
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 hidden md:block font-yekan">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
@@ -27,7 +34,7 @@ const PcNav: React.FC = () => {
         </Link>
 
         {/* Centered Navbar Items */}
-        <div className="flex-1 flex justify-center">
+        <div className="flex justify-center">
           <ul className="font-medium flex p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
@@ -64,14 +71,68 @@ const PcNav: React.FC = () => {
 
         {/* Separate ثبت نام at the end */}
         <div>
-          <ul className="font-medium flex p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li className="flex items-center justify-center mx-4">
-              <IoSearch className="w-6 h-6 text-black text-opacity-80" />
+          <ul className="font-medium flex p-4 md:p-0 mt-4 rounded-lg bg-gray-50 rtl:space-x-reverse md:mt-0 ">
+            <li className="flex items-center justify-center mx-4 min-w-[30vw] xl:min-w-[20vw]">
+              <form
+                className="flex items-center justify-end w-full"
+                onSubmit={(event) => {
+                  event.preventDefault();
+                }}
+              >
+                <label htmlFor="simple-search" className="sr-only">
+                  Search
+                </label>
+                <motion.div
+                  className={`relative w-full ${!searchButton && "hidden"}`}
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{
+                    opacity: searchButton ? 1 : 0,
+                    scale: searchButton ? 1 : 0.8,
+                  }}
+                  transition={{ duration: 0.3, ease: "easeOut" }}
+                >
+                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"></div>
+                  <input
+                    type="text"
+                    id="simple-search"
+                    className="bg-darkGray text-gray text-sm rounded-lg block w-full ps-6 p-2.5 placeholder:text-gray focus:outline-none"
+                    placeholder="جست و جو..."
+                    maxLength={25}
+                  />
+                </motion.div>
+                <motion.button
+                  type="submit"
+                  className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg"
+                  onClick={searchButtonHandler}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <svg
+                    className="w-6 h-6 text-darkGray"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                  <span className="sr-only">Search</span>
+                </motion.button>
+              </form>
             </li>
             <li className="bg-gradient-to-tl from-[#004d00] to-[#00a86b] text-white rounded-2xl py-2 px-6">
               <Link
                 href="/account/signin"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded  md:p-0 "
               >
                 ثبت نام
               </Link>
