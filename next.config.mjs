c61e84c860b6c014782f6bc9bc19e86244f4e4fa
sinaKarimi <karimicna@gmail.com>
2024-12-28 02:02:19 +0330

    successfully configured pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
-// next.config.mjs
+import withPWA from 'next-pwa';
+
+// Use `withPWA` to wrap the Next.js configuration
+const nextConfig = withPWA({
+  dest: 'public', // Output directory for PWA assets
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  register: true, // Automatically registers the service worker
+  skipWaiting: true, // Automatically activates new service worker versions
+});
+
+// Merge with your existing configuration
 export default {
+  ...nextConfig,
   images: {
     remotePatterns: [
       {
